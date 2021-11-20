827a29a905155c306c4ef3336da6ffee96dfe741
nagasivaravuri <ravurishiva1999@gmail.com>
2021-11-20 12:21:42 +0530

    first

---
+++ evn.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    long int n;
+    printf("enter n value ");
+    scanf("%d",&n);
+    if(n%2==0)
+    printf("it is even number ");
+    else 
+    printf("it is odd");
+    return 0;
+}
