7caee591a4df51ca03dfb2b1236dc72dedf5551d
NAGA SIVA KUMAR <94212414+nagasivaravuri@users.noreply.github.com>
2021-11-25 18:42:24 +0530

    Update makefile.h

--- 3_Implementation/makefile.h
+++ 3_Implementation/makefile.h
@@ -1,1 +1,69 @@
+# Name Of the Project
+PROJECT_NAME = Voting system
+
+#All Source code file
+SRC = main.c\
+src/election.c\
+src/election.h\
+
+
+TEST_SRC = src/main.c\
+src/election.c\
+src/election.h\
+test/test.c
+
+#Path to the header file
+INC_HEADER = inc
+INC_TEST = unity
+
+#To check if the OS is windows or linux and set the executable file extension and delete command accordingly
+ifdef OS
+   RM = del /q
+   FixPath = $(subst /,\,$1)
+   EXEC = exe
+else
+   ifeq ($(shell uname), Linux)
+      RM = rm -rf
+      FixPath = $1
+	  EXEC = out
+   endif
+endif
+
+TEST_OUTPUT = Test_$(PROJECT_NAME).$(EXEC)
+
+all:
+	gcc -I $(INC_HEADER) $(SRC) -o $(call FixPath, ./$(PROJECT_NAME).$(EXEC))
+
+run: all
+	$(call FixPath, ./$(PROJECT_NAME).$(EXEC))
+
+test:
+	gcc -I $(INC_HEADER) -I $(INC_TEST) $(TEST_SRC) -o $(call FixPath, ./$(TEST_OUTPUT))
+	$(call FixPath, ./$(TEST_OUTPUT))
+	
+
+coverageCheck:
+	gcc -fprofile-arcs -ftest-coverage -I $(INC_HEADER) -I $(INC_TEST) $(TEST_SRC) -o $(call FixPath, $(TEST_OUTPUT))
+	$(call FixPath, ./$(TEST_OUTPUT))
+	gcov -a $(call FixPath, ./test_Managment.c)
+	$(RM) *.$(EXEC)
+	$(RM) *.gcda
+	$(RM) *.gcno
+	$(RM) *.c.gcov
+	$(RM) *.DAT
+	$(RM) test_Managment.c 
+
+cppcheck:
+	cppcheck --enable=all -I $(INC_HEADER) $(SRC)
+
+valgrind:
+	gcc -I $(INC_HEADER) -I $(INC_TEST) $(TEST_SRC) -o $(call FixPath, ./$(TEST_OUTPUT))
+	valgrind ./$(TEST_OUTPUT)
+
+clean:
+	gcc -fprofile-arcs -ftest-coverage -I $(INC_HEADER) -I $(INC_TEST) $(TEST_SRC) -o $(call FixPath, $(TEST_OUTPUT))
+	$(RM) *.$(EXEC)
+	$(RM) *.gcda
+	$(RM) *.gcno
+	$(RM) *.gcov
 
